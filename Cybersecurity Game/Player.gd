f0c6d3ed9d2c82118bf156c78811340cedb64ca3
ParkourNinjaKitty <68483205+ParkourNinjaKitty@users.noreply.github.com>
2022-05-13 21:34:02 -0700

    Add files via upload

---
+++ Cybersecurity Game/Player.gd
@@ -0,0 +1,24 @@
+extends KinematicBody2D
+
+export (int) var speed = 400
+
+var velocity = Vector2()
+
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+	velocity = velocity.normalized() * speed
+
+func _process(_delta):
+	PlayerStats.playerPos = self.global_position
+
+func _physics_process(_delta):
+	get_input()
+	velocity = move_and_slide(velocity)
---
+++ Cybersecurity Game/PlayerStats.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var playerPos = Vector2(0, 0)
